5599c4279589ae3cf1abf6048b3b3f2ee6fbc601
ryu euni <euni.ryu97@gmail.com>
2023-02-28 16:11:40 +0900

    페어코딩-은이3

--- euni.js
+++ euni.js
@@ -1,3 +1,8 @@
+const Training = {
+  title : "lorem ipsum",
+  paragraph : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  color : ["#1E1E1E","#FFFFFF"],
+  }
 
 function elementMaker(tagName, setAttriName, setAttriValue, parent){
   const elementMake = document.createElement(tagName);
@@ -23,9 +28,28 @@ const cycle = document.getElementById('cycle');
 const title = document.createElement('p');
 box.appendChild(title);
 
+cycle.addEventListener('click', whiteMode);
+function whiteMode(){
+  function maker(tagName,textNode){
+    const element = document.createElement(tagName);
+    element.innerText=textNode;
+    return element;
+    }
 
-title.textContent = training.title;
-paragraph.textContent = training.paragraph;
+let headerDiv =root.appendChild(maker("div",Texts.title));
+let mainDiv=root.appendChild(maker("div",Texts.paragraph));
+let footDiv=root.appendChild(maker("div",""));
+
+root.style.cssText='width:430px;height:932px;'
+headerDiv.style.cssText='position: absolute;width: 147px;height: 29px;left:141px; top:301px; font-style: normal;font-weight: 400;font-size: 24px;line-height: 29px;color: #000000;'
+mainDiv.style.cssText='position: absolute;width: 295px;height: 186px;left: 67px;top: 364px;font-style: normal;font-weight: 400;font-size: 12px;line-height: 15px;text-align: justify;color: #000000;'
+footDiv.style.cssText='position: absolute;width: 82px;height: 82px;left: 174px;top: 673px;background: #5A5A5A;border-radius: 41px;'
+  console.log(":)");
+};
+
+//엘리먼트들 스타일 지정//
+title.textContent = Training.title;
+paragraph.textContent = Training.paragraph;
 
 root.style.width = "430px";
 root.style.height = "932px";
@@ -69,4 +93,4 @@ cycle.style.width = "82px";
 cycle.style.height = "82px";
 cycle.style.borderRadius = "42px";
 cycle.style.backgroundColor = "white";
-
+//------------------------------------------
